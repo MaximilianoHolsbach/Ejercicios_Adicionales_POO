e0b85bf434044b09981825a4974977dc2ae310ec
MaxiCode <andresholsbach@gmail.com>
2025-09-19 00:12:37 -0300

    Se finaliza Producto de ejercicio numero 3

---
+++ Principal_Libro/src/principal_producto/Producto.java
@@ -0,0 +1,89 @@
+package principal_producto;
+//Inventario producto con iva
+public class Producto {
+    private String nombre;
+    private double precio_base;
+    private static double iva = 0.21;
+    private double precio_final;
+    private double precio_descuento;
+    //Constructores
+    public Producto(String nombre,double precio_base){
+        setNombre(nombre);
+        setPrecio_base(precio_base);
+    }
+    //Recargado
+    public Producto(String nombre){
+        setNombre(nombre);
+        this.precio_base = 0.0;
+    }
+    //Metodo
+    public double setAplicar_Descuento(double porcentaje){
+        try {
+            if(porcentaje == 0.0){
+                throw new IllegalArgumentException("El porcentaje ingresado no puede ser cero");
+            }
+            this.precio_descuento = this.precio_base - (this.precio_base * porcentaje);
+            return this.precio_descuento;
+        } catch (Exception e) {
+            System.out.println("Error al establecer el descuento");
+            return 100;
+        }
+    }
+    public double setCalcular_precio_final(){
+        return (this.precio_final + this.precio_descuento) * (1 + Producto.iva);
+    }
+    public static void setCambiar_iva(double nuevo_iva){
+        try {
+            if(nuevo_iva <= 0.0){
+                throw new IllegalArgumentException("El valor del iva no puede ser menor o igual a cero");
+            }
+            Producto.iva = nuevo_iva;
+        } catch (Exception e) {
+            System.out.println("Error al actualizar el iva");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" + "\nNombre = " + nombre + "\nPrecio base = " + precio_base +"\nPrecio final = "+precio_final+ '}';
+    }
+    
+    //Metodo recargado
+    public double setAplicar_Descuento(double porcentaje, double precio_minimo){
+        try {
+            if(porcentaje <= 0.0 || precio_minimo == 0.0){
+                throw new IllegalArgumentException("Los valores solicitados no pueden ser menores o igual a cero");
+            }
+            if(setAplicar_Descuento(porcentaje) > precio_minimo){
+                this.precio_descuento = setAplicar_Descuento(porcentaje);
+                return this.precio_descuento;
+            }
+            this.precio_descuento = precio_minimo;
+            return this.precio_descuento;
+        } catch (Exception e) {
+            System.out.println("Error al aplicar descuento");
+            return this.precio_descuento;
+        }
+    }
+    //Metodo privados
+    private void setNombre(String nombre){
+        try {
+            if(nombre == null || nombre.trim().isEmpty()){
+                throw new IllegalArgumentException("El nombre del producto no puede estar vacio");
+            }
+            this.nombre = nombre;
+        } catch (Exception e) {
+            System.out.println("Error al establecer nombre del producto");
+        }
+    }
+    private void setPrecio_base(double precio){
+        try {
+            if(precio <= 0.0){
+                throw new IllegalArgumentException("El precio del producto no puede ser menor o igual a cero");
+            }
+            this.precio_base = precio;
+        } catch (Exception e) {
+            System.out.println("Error al establecer precio del producto");
+        }
+    }
+}
